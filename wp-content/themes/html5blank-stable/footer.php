e54e0a6012d29483bc6f4542c56e0b56b2b5542f
Jeremy Evans <guest2@Tim-Meldrums-MacBook-Pro.local>
2016-04-24 12:06:10 +0100

    Now with jQuery

--- wp-content/themes/html5blank-stable/footer.php
+++ wp-content/themes/html5blank-stable/footer.php
@@ -26,8 +26,10 @@
 		ga('send', 'pageview');
 		</script>
 
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js"></script>
 
 		<!-- Tabs script -->
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.4/jquery.touchSwipe.min.js"></script>
 		<script>
 		/*my lovely util functions*/
 /*active class*/
