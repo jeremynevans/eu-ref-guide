d04b31f25d721fc9d42182d5e7a801e792bb41de
Jeremy Evans <guest2@Tim-Meldrums-MacBook-Pro.local>
2016-04-24 12:11:22 +0100

    More tabs

--- wp-content/themes/html5blank-stable/page-issues.php
+++ wp-content/themes/html5blank-stable/page-issues.php
@@ -16,6 +16,9 @@ The Issues!
           <li class="active"><a href="#">Foo</a></li>
           <li><a href="#">Bar</a></li>
           <li><a href="#">Thunder</a></li>
+          <li><a href="#">Bar</a></li>
+          <li><a href="#">Thunder</a></li>
+          <li><a href="#">Bar</a></li>
       </ul>
     </div>
   </nav>
@@ -33,6 +36,21 @@ The Issues!
         <div class='col-md-10 col-md-offset-1 ratio-2:1 inner'>
           <?php get_template_part('loop'); ?>
         </div>
+      </div>
+      <div class="content">
+          <div class='col-md-10 col-md-offset-1 ratio-2:1 inner'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </div>
+      </div>
+      <div class="content">
+          <div class='col-md-10 col-md-offset-1 ratio-2:1 inner'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </div>
+      </div>
+      <div class="content">
+          <div class='col-md-10 col-md-offset-1 ratio-2:1 inner'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </div>
       </div>
       <div class="content">
           <div class='col-md-10 col-md-offset-1 ratio-2:1 inner'>
