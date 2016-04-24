bead668bdefd31e260c15989bbaa8ee5d349b913
Jeremy Evans <guest2@Tim-Meldrums-MacBook-Pro.local>
2016-04-24 11:58:46 +0100

    Issues page on homepage with tabs

---
+++ wp-content/themes/html5blank-stable/page-issues.php
@@ -0,0 +1,61 @@
+<?php get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+The Issues!
+
+			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+
+
+<div class="layer">
+  <nav class="navbar navbar-default slide-tab" role="navigation">
+    <div class="container-fluid scroll invisibar">
+        <ul class="nav navbar-nav breakscroll">
+          <li class="active"><a href="#">Foo</a></li>
+          <li><a href="#">Bar</a></li>
+          <li><a href="#">Thunder</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="hotspot">
+    <div class="half slide" data="left">
+      <button class="click"></button>
+    </div>
+    <div class="half slide" data="right">
+      <button class="click"></button>
+    </div>
+  </div>
+  <div class="dash scroll invisibar">
+    <div class="panes breakscroll">
+      <div class="content">
+        <div class='col-md-10 col-md-offset-1 ratio-2:1 inner'>
+          <?php get_template_part('loop'); ?>
+        </div>
+      </div>
+      <div class="content">
+          <div class='col-md-10 col-md-offset-1 ratio-2:1 inner'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </div>
+      </div>
+      <div class="content">
+          <div class='col-md-10 col-md-offset-1 ratio-2:1 inner'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+			<!-- <?php get_template_part('pagination'); ?> -->
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
